c5f8e6a7d66d72c27d14c372996b58ad296f5010
Ilya Otpuschennikov <otilya@yandex.ru>
2020-03-16 12:09:08 +0800

    minisat muiltithread util

--- src/test.c
+++ src/test.c
@@ -46,21 +46,43 @@ void task(int j)
         system(tmp_3.c_str());
 }
 
+void minisat(int j)
+{
+    std::cout << "task " << j << " starts: " << std::endl;
+    // std::cout << chrono::system_clock::now().time_since_epoch().count() << std::endl;
+		std::stringstream command_solve;
+        command_solve << "./minisat_core -verb=0 test_simp_with_10_zero_hash_bits.cnf res_" << j << " iter=" << j << " > log_" << j;
+        const std::string tmp_3 =  std::string{command_solve.str()};
+        system(tmp_3.c_str());
+}
+
 
 int main(int argc, char *argv[]) {
 
 
-	// #pragma omp parallel for
+	// // #pragma omp parallel for
 	// for (uif32 j = 0; j < cores; ++j) {
 	// 	// printf("%d core\n", j);
 		
 	// }
 
-	thread t1(task, 1);
-	thread t2(task, 2);
+	thread t1(minisat, 1);
+	thread t2(minisat, 2);
+	thread t3(minisat, 3);
+	thread t4(minisat, 4);
+	thread t5(minisat, 5);
+	thread t6(minisat, 6);
+	thread t7(minisat, 7);
+	thread t8(minisat, 8);
 
 	t1.join();
 	t2.join();
+	t3.join();
+	t4.join();
+	t5.join();
+	t6.join();
+	t7.join();
+	t8.join();
 
 	return 1;
 
